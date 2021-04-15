a8c7c4a0d8dfa6ec84a99e49dd94dff3c9760ffe
Przemyslaw Mejna <pmejna@gmail.com>
2021-04-15 16:01:17 +0100

    create contact styled components

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import logoImg from '../../assets/images/Logo.svg';
+import phoneIcon from '../../assets/images/PhoneIcon.svg';
+import footerBg from '../../assets/images/ContactGraphic.png';
 
 const ContactSection = styled.section`
     padding: var(--main-padding);
@@ -15,21 +17,63 @@ const ContactFooter = styled.footer`
     height: auto;
 `;
 
+const FooterHead = styled.div`
+    width: 100%;
+    background-color: #F1F1F1;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: var(--main-padding);
+    min-height: 7rem;
+`;
+
 const Logo = styled.img`
     width: 11rem;
     height: auto;
     background-color: transparent;
 `;
 
-const FooterHead = styled.div`
-width: 100%;
-background-color: #000;
+const PhoneNumbers = styled.div`
+    width: 320px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    a {
+        text-decoration: none;
+        color: var(--main-dark);
+        font-size: 1.8rem;
+        font-weight: 500;
+    }
 `;
 
-const FooterData = styled.div`
-width: 100%;
-background-color: #000;
 
+
+const FooterData = styled.div`
+    position: relative;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 1fr 2fr;
+    grid-gap: 2rem;
+    padding: var(--main-padding);
+    padding-top: 3rem;
+    background-color: #000;
+    color: #fff;
+    
+`;
+
+
+const FooterDataBox = styled.div`
+    h4 {
+        font-size: 1.8rem;
+    }
+
+    span {
+        font-weight: 500;
+    }
 `;
 
 const Footer = styled.div`
@@ -55,8 +99,15 @@ background-color: #fff;
 
 `;
 
+const FooterImage = styled.img`
+    position: absolute;
+    bottom: 1rem;
+    left: 0;
+`;
+
 const Contact = () => {
-    const today = new Date();
+    const today = new Date().getFullYear();
+    console.log(today);
     return (
         <>
             <ContactSection id='kontakt'>
@@ -66,15 +117,37 @@ const Contact = () => {
             <ContactFooter>
                 <FooterHead>
                     <Logo src={logoImg}/>
-                    <a href="+44774564634">0774 564 634</a>
+                    <PhoneNumbers>
+                        <img src={phoneIcon} alt=""/>
+                        <a href="+44774564634">0774 564 634</a>
+                        <a href="+44774564634">0774 564 634</a>
+                    </PhoneNumbers>
                 </FooterHead>
                 <FooterData>
-
+                    <FooterDataBox>
+                        <h4>Adres</h4>
+                        <p>ul. Sadowa 1</p>
+                        <p>05-090 Sękocin Nowy</p>
+                    </FooterDataBox>
+                    <FooterDataBox>
+                        <h4>Email</h4>
+                        <p>biuro@internett.pl</p>
+                    </FooterDataBox>
+                    <FooterDataBox>
+                        <h4>Nip</h4>
+                        <p>522-174-62-78</p>
+                    </FooterDataBox>
+                    <FooterDataBox>
+                        <h4>Godziny pracy:</h4>
+                        <p>poniedziałek-piątek: <span>8 - 16</span></p>
+                        <p>sobota: <span>10 - 14</span></p>
+                    </FooterDataBox>
+                    <FooterImage src={footerBg}></FooterImage>
                 </FooterData>
                 <Footer>
                     <div className="redBar"></div>
                     <div className="footer">
-                        <p className="copyright">Copyright {today.getFullYear}</p>
+                        <p className="copyright">Copyright {today}</p>
                         <p>www.internett.pl</p>
                     </div>
                 </Footer>
--- src/pages/dladomu.js
+++ src/pages/dladomu.js
@@ -3,8 +3,12 @@ import styled from 'styled-components';
 import MainSlide from '../components/MainSlide/MainSlide';
 import VerticalSection from '../components/VerticalSection/VerticalSection';
 import bgImage from '../assets/images/DlaDomu.jpeg';
+import lines1 from '../assets/images/Group 15.png';
+import Lines01 from "../components/Lines/Lines01";
+import fibreImg from '../assets/images/fibre001.jpeg';
 
 import {data} from '../assets/texts/dlaDomuText';
+import Button from '../components/Button/Button';
 
 const HeadSection = styled.section`
     padding: var(--main-padding);
@@ -17,6 +21,58 @@ const HeadSection = styled.section`
     }
 `;
 
+const FibreSection = styled.section`
+    width: 100%;
+    height: auto;
+    padding: 0;
+    margin-bottom: 8rem;
+
+    h2 {
+        text-align: center;
+        padding: var(--main-padding);
+    }
+`;
+const FibreImageSection = styled.section`
+    width: 100%;
+    min-height: 500px;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url(${fibreImg});
+    background-size: 100%;
+    color: #fff;
+
+    p {
+        text-align: center;
+    }
+`;
+const BarButton = styled(Button)`
+    margin: 0;
+`;
+
+const FibreImageBar = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 8rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.6rem 12vw;
+    background-color: #fff;
+    color: #000;
+    border: 1px solid #e1e1e1;
+`;
+
+const TvSection = styled.section`
+
+`;
+
+
+
 const DlaDomuPage = () => {
     return (
         <>
@@ -26,8 +82,24 @@ const DlaDomuPage = () => {
                 <p>{`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores.`}</p>
             </HeadSection>
-            <VerticalSection box_data={data.box_data} />
-            <VerticalSection box2_data={data.box2_data}/>
+            <FibreSection>
+                <h2>Oferta internetu światłowodowego</h2>
+                <FibreImageSection>
+                    <p> Oferujemy budowę oraz podłączenie do internetu światłowodowego oraz rozprowadzenie internetu bezprzewodowego po całej firmie aby jego sygnał był stabilny i silny w całej siedzibie firmy. Rozpoczynając z nami współpracę nie będziesz musiał martwić się fatalnej jakości połączeniami z klientem.</p>
+                    <FibreImageBar>
+                        <p>100Mb/s juz od: <span>99zł</span></p>
+                        <BarButton />
+                    </FibreImageBar>
+                </FibreImageSection>
+            </FibreSection>
+            <Lines01 image={lines1} alt="superszybki internet"/>
+            <TvSection>
+                <h2></h2>
+                <figure>
+                    <img></img>
+                </figure>
+                <p></p>
+            </TvSection>
         </>
     )
 };
