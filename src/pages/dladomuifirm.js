addb627da2f96a59257dc5c3844bbcbdd75cebc7
Przemyslaw Mejna <pmejna@gmail.com>
2021-05-16 13:54:18 +0100

    update 2

--- src/pages/dladomuifirm.js
+++ src/pages/dladomuifirm.js
@@ -130,9 +130,9 @@ const FibreImageBar = styled.div`
 const DlaDomuPage = () => {
     return (
         <>
-            <MainSlide mainText='Internet dla domu' image={bgImage}/>
+            <MainSlide mainText='Internet dla domu i firm' image={bgImage}/>
             <HeadSection
-                header="Internet światłowodowy dla domu i firm"
+                header="Internet światłowodowy dla domu"
                 paragraph={`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores.`}
             />
